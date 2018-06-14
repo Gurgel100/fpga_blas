ad5c5684094c89ecc821439f5b63b826b850821d
Pascal Störzbach <stpascal@student.ethz.ch>
2018-06-14 20:59:30 +0200

    Fixed dot product for vectors of arbitrary length

--- include/Core.h
+++ include/Core.h
@@ -18,12 +18,12 @@ public:
 	Core &operator=(Core &&) = delete;
 
 	template <size_t num_partial_sums>
-	static void macc_step(Stream<Data_t> &in_X, Stream<Data_t> &in_Y, Data_t part_sums[num_partial_sums], const size_t index, const size_t round) {
+	static void macc_step(Stream<Data_t> &in_X, Stream<Data_t> &in_Y, Data_t part_sums[num_partial_sums], const size_t index, const size_t round, const size_t N) {
 		#pragma HLS INLINE
-		#pragma HLS PIPELINE II=1
+		size_t currentN = round * num_partial_sums + index;
 		Data_t sum = (round == 0) ? 0 : part_sums[index];
-		Data_t x = in_X.Pop();
-		Data_t y = in_Y.Pop();
+		Data_t x = currentN < N ? in_X.Pop() : 0;
+		Data_t y = currentN < N ? in_Y.Pop() : 0;
 		part_sums[index] = sum + x * y;
 		#pragma HLS DEPENDENCE variable=part_sums inter false
 	}
@@ -34,13 +34,14 @@ public:
 		Data_t part_sums[num_partial_sums];
 
 		const size_t rounds = N / num_partial_sums;
-		macc_loop: for (size_t round = 0; round < rounds; ++round) {
-			#pragma HLS PIPELINE II=1
+		macc_loop: for (size_t round = 0; round <= rounds; ++round) {
 			for (size_t i = 0; i < num_partial_sums; ++i) {
-				macc_step<num_partial_sums>(in_X, in_Y, part_sums, i, round);
+				#pragma HLS PIPELINE II=1
+				macc_step<num_partial_sums>(in_X, in_Y, part_sums, i, round, N);
 			}
 		}
 		for (size_t i = 0; i < num_partial_sums; ++i) {
+			#pragma HLS PIPELINE II=1
 			Data_t sum = 0;
 			if (i < N) {
 				sum = part_sums[i];
