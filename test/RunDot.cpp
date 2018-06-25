1618bf706fb55b1ee616ffde0a45d3b5ed7dbbbe
Pascal Störzbach <stpascal@student.ethz.ch>
2018-06-25 18:45:38 +0200

    Fixed FPGA test and added test for interleaved dot

--- test/RunDot.cpp
+++ test/RunDot.cpp
@@ -6,6 +6,7 @@
 #include <iostream>
 #include <vector>
 #include <random>
+#include <strstream>
 #define CATCH_CONFIG_MAIN
 #include <catch.hpp>
 
@@ -25,16 +26,13 @@ template <typename T>
 static void test_dot(ocl::Context &context, const std::string &kernelFile, const std::vector<size_t> &sizes) {
 	const size_t maxSize = sizes.back();
 
-	std::cout << "Initializing device memory..." << std::flush;
-	auto inputDeviceX = context.MakeBuffer<T, ocl::Access::read>(ocl::MemoryBank::bank0, maxSize * sizeof(T));
-	auto inputDeviceY = context.MakeBuffer<T, ocl::Access::read>(ocl::MemoryBank::bank1, maxSize * sizeof(T));
-	auto outputDevice = context.MakeBuffer<T, ocl::Access::write>(ocl::MemoryBank::bank0, sizeof(T));
-	std::cout << " Done." << std::endl;
+	auto inputDeviceX = context.MakeBuffer<T, ocl::Access::read>(ocl::MemoryBank::bank0, maxSize);
+	auto inputDeviceY = context.MakeBuffer<T, ocl::Access::read>(ocl::MemoryBank::bank0, maxSize);
+	auto outputDevice = context.MakeBuffer<T, ocl::Access::write>(ocl::MemoryBank::bank0, 1);
 
-	std::cout << "Initialising input data..." << std::flush;
 	std::vector<T> inputHostX(maxSize);
 	std::vector<T> inputHostY(maxSize);
-	std::vector<T> outputHost(1);
+	std::array<T, 1> outputHost;
 	std::random_device rd;
 	std::default_random_engine re(rd());
 	std::uniform_real_distribution<T> dist(-10, 10);
@@ -43,45 +41,102 @@ static void test_dot(ocl::Context &context, const std::string &kernelFile, const
 		inputHostX[i] = dist(re);
 		inputHostY[i] = dist(re);
 	}
-	std::cout << " Done." << std::endl;
 
-	std::cout << "Copying input to device..." << std::flush;
 	inputDeviceX.CopyFromHost(inputHostX.data());
 	inputDeviceY.CopyFromHost(inputHostY.data());
-	std::cout << " Done." << std::endl;
 
-	std::cout << "Creating program..." << std::flush;
-	auto program = context.MakeProgram(kernelFile);
-	std::cout << " Done." << std::endl;
+	auto program = context.MakeProgram(kernelFile + ".xclbin");
 
 	for (auto &size : sizes) {
-		std::cout << "Executing kernel for size " << size << "..." << std::flush;
-		auto kernel = program.MakeKernel("blas_dot", inputDeviceX, 1, inputDeviceY, 1, outputDevice, size);
-		auto elapsed = kernel.ExecuteTask();
-		std::cout << " Done." << std::endl;
+		SECTION("Vector of size " + std::to_string(size)) {
+			auto kernel = program.MakeKernel("blas_dot", size, inputDeviceX, 1, inputDeviceY, 1, outputDevice);
+			kernel.ExecuteTask();
 
-		std::cout << "Kernel ran in " << elapsed.first << " seconds.\n";
+			outputDevice.CopyToHost(outputHost.data());
+			auto reference = reference_dot<T>(inputHostX, inputHostY, size);
+			REQUIRE(outputHost[0] == Approx(reference));
+		}
+	}
+}
 
-		std::cout << "Verifying result..." << std::flush;
-		outputDevice.CopyToHost(outputHost.data());
-		REQUIRE(outputHost[0] == reference_dot(inputHostX, inputHostY, size));
-		std::cout << " Done." << std::endl;
+template <typename T>
+static void test_interleaved_dot(ocl::Context &context, const std::string &kernelFile, const std::vector<size_t> &sizes, const std::vector<size_t> &num_vectors) {
+	const size_t maxSize = sizes.back();
+	const size_t maxVectors = num_vectors.back();
 
-		std::cout << "Kernel ran successfully." << std::endl;
+	auto inputDeviceX = context.MakeBuffer<T, ocl::Access::read>(ocl::MemoryBank::bank0, maxVectors * maxSize);
+	auto inputDeviceY = context.MakeBuffer<T, ocl::Access::read>(ocl::MemoryBank::bank0, maxVectors * maxSize);
+	auto outputDevice = context.MakeBuffer<T, ocl::Access::write>(ocl::MemoryBank::bank0, maxVectors);
+
+	std::vector<T> inputHostX(maxVectors * maxSize);
+	std::vector<T> inputHostY(maxVectors * maxSize);
+	std::vector<T> outputHost(maxVectors);
+	std::random_device rd;
+	std::default_random_engine re(rd());
+	std::uniform_real_distribution<T> dist(-10, 10);
+
+	for (size_t i = 0; i < maxVectors * maxSize; ++i) {
+		inputHostX[i] = dist(re);
+		inputHostY[i] = dist(re);
+	}
+
+	inputDeviceX.CopyFromHost(inputHostX.data());
+	inputDeviceY.CopyFromHost(inputHostY.data());
+
+	auto program = context.MakeProgram(kernelFile + ".xclbin");
+
+	for (auto &num_vector : num_vectors) {
+		SECTION(std::to_string(num_vector) + " vectors") {
+			for (auto &size : sizes) {
+				SECTION("Vectors of size " + std::to_string(size)) {
+					auto kernel = program.MakeKernel("blas_dot_multiple", size, num_vector, inputDeviceX, inputDeviceY,
+					                                 outputDevice);
+					kernel.ExecuteTask();
+
+					outputDevice.CopyToHost(outputHost.data());
+					for (size_t i = 0; i < num_vector; ++i) {
+						INFO("Vector " << i + 1);
+						auto beginRefX = inputHostX.begin() + i * size;
+						auto beginRefY = inputHostY.begin() + i * size;
+
+						auto refX = std::vector<T>(beginRefX, beginRefX + size);
+						auto refY = std::vector<T>(beginRefY, beginRefY + size);
+						auto reference = reference_dot<T>(refX, refY, size);
+						REQUIRE(outputHost[i] == Approx(reference));
+					}
+				}
+			}
+		}
 	}
 }
 
 TEST_CASE("Dot", "[Dot]") {
 	std::vector<size_t> sizes;
-	for (size_t i = 0; i < 32; ++i) {
+	for (size_t i = 1; i <= 32; ++i) {
 		sizes.push_back(i);
 	}
 
-	std::cout << "Initializing OpenCL context..." << std::endl;
 	ocl::Context context;
-	std::cout << "Done." << std::endl;
 
-	SECTION("Double") {
-		test_dot<double>(context, "kernel_dot", sizes);
+	SECTION("Float") {
+		test_dot<float>(context, "kernel_dot", sizes);
+	}
+}
+
+TEST_CASE("Interleaved Dot", "[Dot]") {
+	std::vector<size_t> sizes;
+	std::vector<size_t> num_vectors;
+
+	for (size_t i = 1; i <= 32; ++i) {
+		sizes.push_back(i);
+	}
+	for (size_t i = 1; i <= 32; ++i) {
+		num_vectors.push_back(i);
+	}
+
+	ocl::Context context;
+
+	SECTION("Float") {
+		test_interleaved_dot<float>(context, "kernel_dot_multiple", sizes, num_vectors);
 	}
 }
