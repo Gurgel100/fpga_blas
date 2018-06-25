5ded186499d982dc366093c9ba30d8e45359a9df
Pascal Störzbach <stpascal@student.ethz.ch>
2018-06-25 18:44:16 +0200

    Reworked test program so that it now uses the Catch test framework

--- test/TestProgram.cpp
+++ test/TestProgram.cpp
@@ -3,47 +3,68 @@
 #include <vector>
 #include <random>
 #include <cassert>
-#include "Kernel.h"
+#define CATCH_CONFIG_MAIN
+#include <catch.hpp>
+#include "DotProduct.h"
 
 using namespace std;
 
 template <class T>
-static void printResult(T expected, T value) {
-	if (abs(value - expected) > 1e-3) {
-		cout << "Mismatch: " << value << " (expected " << expected << ')' << endl;
-	} else {
-		cout << "Got correct result" << endl;
+static void fill(vector<T> &d) {
+	static mt19937 mt;
+	uniform_real_distribution<Data_t> dist(0.0, 10.0);
+
+	for (auto &item : d) {
+		item = dist(mt);
 	}
 }
 
 template <class T>
-static T reference_dot(const vector<T> &x, const vector<T> &y)
+static T reference_dot(const size_t N, const vector<T> &x, const vector<T> &y)
 {
-	assert(x.size() == y.size());
 	T res = 0;
-	for (size_t i = 0; i < x.size(); ++i) {
+	for (size_t i = 0; i < N; ++i) {
 		res += x[i] * y[i];
 	}
 	return res;
 }
 
-int main(int argc, char const *argv[]) {
-	uniform_real_distribution<Data_t> dist(0.0, 10.0);
-	mt19937 mt;
-	for (size_t n = 1; n < 64; ++n) {
-		cout << "Testing for n = " << n << ": ";
-		vector<Data_t> x(n);
-		vector<Data_t> y(n);
-		for (size_t i = 0; i < n; ++i) {
-			x[i] = dist(mt);
-			y[i] = dist(mt);
+TEST_CASE("blas_dot") {
+	const size_t maxSize = 32;
+	vector<Data_t> x(maxSize), y(maxSize), out(1);
+	fill(x);
+	fill(y);
+
+	for (size_t n = 1; n < maxSize; ++n) {
+		SECTION("Vectors of size " + to_string(n)) {
+			Data_t ref = reference_dot(n, x, y);
+			blas_dot(n, x.data(), 1, y.data(), 1, out.data());
+			REQUIRE(out[0] == Approx(ref));
 		}
-
-		Data_t ref = reference_dot(x, y);
-
-		vector<Data_t> out(1, 0);
-		blas_dot(x.data(), 1, y.data(), 1, out.data(), n);
-		printResult(ref, out[0]);
 	}
-	return 0;
+}
+
+TEST_CASE("blas_dot_multiple") {
+	const size_t maxSize = 32;
+	const size_t maxVectors = 32;
+
+	for (size_t i = 1; i < maxVectors; ++i) {
+		SECTION(to_string(i) + " vectors") {
+			vector<Data_t> x(maxSize * i), y(maxSize * i), out(i);
+			fill(x);
+			fill(y);
+			for (size_t n = 1; n < maxSize; ++n) {
+				SECTION("Vectors of size " + to_string(n)) {
+					blas_dot_multiple(n, i, x.data(), y.data(), out.data());
+					for (size_t j = 0; j < i; ++j) {
+						auto tmpx = vector<Data_t>(x.begin() + j * n, x.begin() + j * n + n);
+						auto tmpy = vector<Data_t>(y.begin() + j * n, y.begin() + j * n + n);
+						Data_t reference = reference_dot(n, tmpx, tmpy);
+						INFO("Vector " << j + 1);
+						REQUIRE(out[j] == Approx(reference));
+					}
+				}
+			}
+		}
+	}
 }
