4745eecae75428b5bb7a78e9d2c8cc1c952442f5
Pascal Störzbach <stpascal@student.ethz.ch>
2018-06-25 16:20:20 +0200

    Added classes to read and write from memory

--- include/Memory.h
+++ include/Memory.h
@@ -5,11 +5,80 @@
 #ifndef BLAS_HLS_MEMORY_H
 #define BLAS_HLS_MEMORY_H
 
-#include "hlslib/Stream.h"
+#include <hlslib/Stream.h>
+#include <hlslib/Simulation.h>
 
 namespace Memory {
 
 	using hlslib::Stream;
+
+	template <class T>
+	class MemoryReader {
+	public:
+		MemoryReader(Stream<T> &pipe, const size_t N, const size_t increment) : pipe(pipe), N(N), increment(increment) {
+			#pragma HLS INLINE
+		}
+
+		template <bool dataflow = false>
+		void readFromMemory(const T memory[]) {
+			#pragma HLS INLINE
+			if (dataflow) {
+				HLSLIB_DATAFLOW_FUNCTION(readFromMemory, N, increment, memory, pipe);
+			} else {
+				readFromMemory(N, increment, memory, pipe);
+			}
+		}
+
+	protected:
+		Stream<T> &pipe;
+		const size_t N, increment;
+
+	private:
+		static void readFromMemory(const size_t N, const size_t increment, const T memory[], Stream<T> &pipe) {
+			#pragma HLS INLINE
+			size_t currentLoc = 0;
+			MemoryReader_read_loop:
+			for (size_t i = 0; i < N; ++i) {
+				#pragma HLS PIPELINE II=1
+				pipe.Push(memory[currentLoc]);
+				currentLoc += increment;
+			}
+		}
+	};
+
+	template <class T>
+	class MemoryWriter {
+	public:
+		MemoryWriter(Stream<T> &pipe, const size_t N, const size_t increment) : pipe(pipe), N(N), increment(increment) {
+			#pragma HLS INLINE
+		}
+
+		template <bool dataflow = false>
+		void writeToMemory(T memory[]) {
+			#pragma HLS INLINE
+			if (dataflow) {
+				HLSLIB_DATAFLOW_FUNCTION(writeToMemory, N, increment, memory, pipe);
+			} else {
+				writeToMemory(N, increment, memory, pipe);
+			}
+		}
+
+	protected:
+		Stream<T> &pipe;
+		const size_t N, increment;
+
+	private:
+		static void writeToMemory(const size_t N, const size_t increment, T memory[], Stream<T> &pipe) {
+			#pragma HLS INLINE
+			size_t currentLoc = 0;
+			MemoryWriter_write_loop:
+			for (size_t i = 0; i < N; ++i) {
+				#pragma HLS PIPELINE II=1
+				memory[currentLoc] = pipe.Pop();
+				currentLoc += increment;
+			}
+		}
+	};
 
 	template<class T>
 	void ReadMemory(const T memory[], Stream<T> &pipe, const size_t N, const int increment) {
