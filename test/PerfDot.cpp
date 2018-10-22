df33abd3ccfa98bec8a53f7ec6caf9214f68a99e
Pascal Störzbach <stpascal@student.ethz.ch>
2018-10-22 18:28:29 +0200

    Removed accidentally committed code

--- test/PerfDot.cpp
+++ test/PerfDot.cpp
@@ -39,8 +39,7 @@ private:
 	double freq;
 };
 
-//const auto freq = Frequency<mega>(300.0);
-const auto freq = Frequency<mega>(206.9);
+const auto freq = Frequency<mega>(300.0);
 
 int main(int argc, const char *argv[]) {
 	const size_t size = static_cast<size_t>(argc > 1 ? strtod(argv[1], NULL) : 5e8);
@@ -48,21 +47,11 @@ int main(int argc, const char *argv[]) {
 		cerr << "Size must be divisible by 16!" << endl;
 		return 1;
 	}
-	if (size % 1000 != 0) {
-		cerr << "Size must be divisible by 1000!" << endl;
-		return 1;
-	}
-	const size_t chunkwidth = static_cast<size_t>(argc > 2 ? strtod(argv[2], NULL) : 1) * 16;
-	if (size % chunkwidth != 0) {
-		cerr << "Size must be divisible by chunkwidth!" << endl;
-		return 1;
-	}
 
 	vector<Type_t> inputHostX(size, 2), inputHostY(size, 1);
 	array<Type_t, 1> outputHost = {0};
 
 	cout << "size = " << size << endl;
-	cout << "chunkwidth = " << chunkwidth << endl;
 
 	ocl::Context context;
 
@@ -74,10 +63,7 @@ int main(int argc, const char *argv[]) {
 
 	cout << "Actual frequency: " << freq.getFreq<mega>() << " MHz" << endl;
 
-	this_thread::sleep_for(chrono::seconds(1));
-
-	auto kernel = program.MakeKernel("blas_dot", size, chunkwidth, inputDeviceX, inputDeviceY, outputDevice);
-	this_thread::sleep_for(chrono::milliseconds(1000));
+	auto kernel = program.MakeKernel("blas_dot", size, inputDeviceX, inputDeviceY, outputDevice);
 	cout << "Executing kernel..." << endl;
 	auto time = kernel.ExecuteTask();
 
