986ad930ef3c52f10ef1015c50883aa1f3063859
Pascal Störzbach <stpascal@student.ethz.ch>
2018-10-25 14:30:01 +0200

    Added testcases for gemv and transposed gemv

--- test/TestProgram.cpp
+++ test/TestProgram.cpp
@@ -6,6 +6,7 @@
 #define CATCH_CONFIG_MAIN
 #include <catch.hpp>
 #include "DotProduct.h"
+#include "GEMV.h"
 #include "cblas.h"
 
 using namespace std;
@@ -89,6 +90,134 @@ TEST_CASE("blas_dot_multiple") {
 						Data_t reference = reference_dot(n * dot_width, tmpx, tmpy);
 						INFO("Vector " << j + 1);
 						REQUIRE(out[j] == Approx(reference));
+					}
+				}
+			}
+		}
+	}
+}
+
+TEST_CASE("blas_gemv") {
+
+	for (size_t N = 32; N <= 64; N += 32) {
+		for (size_t M = 16; M <= 48; M += 16) {
+			vector<Data_t> A(N * M);
+			vector<Data_t> x(N);
+			vector<Data_t> y(M);
+			vector<Data_t> ref_y(M);
+			
+			SECTION("N=" + to_string(N) + ", M=" + to_string(M)) {
+				SECTION("All 1s") {
+					fill(A.begin(), A.end(), 1);
+					fill(x.begin(), x.end(), 1);
+
+					blas_gemv(reinterpret_cast<FBLAS::MatrixVectorMultiplication<Data_t>::Col_t *>(A.data()),
+					          reinterpret_cast<FBLAS::MatrixVectorMultiplication<Data_t>::Col_t *>(x.data()),
+					          y.data(), N, M);
+
+					cblas_sgemv(CblasRowMajor, CblasNoTrans, M, N, 1, A.data(), N, x.data(), 1, 0, ref_y.data(), 1);
+
+					for (size_t i = 0; i < M; ++i) {
+						INFO("Y[" << i << "]");
+						CHECK(y[i] == Approx(ref_y[i]));
+					}
+				}
+
+				SECTION("Counted") {
+					iota(A.begin(), A.end(), 0);
+					iota(x.begin(), x.end(), 0);
+
+					blas_gemv(reinterpret_cast<FBLAS::MatrixVectorMultiplication<Data_t>::Col_t *>(A.data()),
+					          reinterpret_cast<FBLAS::MatrixVectorMultiplication<Data_t>::Col_t *>(x.data()),
+					          y.data(), N, M);
+
+					cblas_sgemv(CblasRowMajor, CblasNoTrans, M, N, 1, A.data(), N, x.data(), 1, 0, ref_y.data(), 1);
+
+					for (size_t i = 0; i < M; ++i) {
+						INFO("Y[" << i << "]");
+						CHECK(y[i] == Approx(ref_y[i]));
+					}
+				}
+
+				SECTION("Random") {
+					mt19937 g;
+					uniform_real_distribution<Data_t> dist;
+					generate(A.begin(), A.end(), [&g, &dist](){return dist(g);});
+					generate(x.begin(), x.end(), [&g, &dist](){return dist(g);});
+
+					blas_gemv(reinterpret_cast<FBLAS::MatrixVectorMultiplication<Data_t>::Col_t *>(A.data()),
+					          reinterpret_cast<FBLAS::MatrixVectorMultiplication<Data_t>::Col_t *>(x.data()),
+					          y.data(), N, M);
+
+					cblas_sgemv(CblasRowMajor, CblasNoTrans, M, N, 1, A.data(), N, x.data(), 1, 0, ref_y.data(), 1);
+
+					for (size_t i = 0; i < M; ++i) {
+						INFO("Y[" << i << "]");
+						CHECK(y[i] == Approx(ref_y[i]));
+					}
+				}
+			}
+		}
+	}
+}
+
+TEST_CASE("blas_gemv_transposed") {
+
+	for (size_t N = 32; N <= 64; N += 32) {
+		for (size_t M = 16; M <= 48; M += 16) {
+			vector<Data_t> A(N * M);
+			vector<Data_t> x(M);
+			vector<Data_t> y(N);
+			vector<Data_t> ref_y(N);
+
+			SECTION("N=" + to_string(N) + ", M=" + to_string(M)) {
+				SECTION("All 1s") {
+					fill(A.begin(), A.end(), 1);
+					fill(x.begin(), x.end(), 1);
+
+					blas_gemv_transposed(reinterpret_cast<FBLAS::MatrixVectorMultiplicationTransposed<Data_t, 16, 32, 16>::Col_t *>(A.data()), x.data(),
+					          reinterpret_cast<FBLAS::MatrixVectorMultiplicationTransposed<Data_t, 16, 32, 16>::Col_t *>(y.data()),
+					          N, M);
+
+					cblas_sgemv(CblasRowMajor, CblasTrans, M, N, 1, A.data(), N, x.data(), 1, 0, ref_y.data(), 1);
+
+					for (size_t i = 0; i < N; ++i) {
+						INFO("Y[" << i << "]");
+						CHECK(y[i] == Approx(ref_y[i]));
+					}
+				}
+
+				SECTION("Counted") {
+					iota(A.begin(), A.end(), 0);
+					iota(x.begin(), x.end(), 0);
+
+					blas_gemv_transposed(reinterpret_cast<FBLAS::MatrixVectorMultiplicationTransposed<Data_t, 16, 32, 16>::Col_t *>(A.data()), x.data(),
+					                     reinterpret_cast<FBLAS::MatrixVectorMultiplicationTransposed<Data_t, 16, 32, 16>::Col_t *>(y.data()),
+					                     N, M);
+
+					cblas_sgemv(CblasRowMajor, CblasTrans, M, N, 1, A.data(), N, x.data(), 1, 0, ref_y.data(), 1);
+
+					for (size_t i = 0; i < N; ++i) {
+						INFO("Y[" << i << "]");
+						CHECK(y[i] == Approx(ref_y[i]));
+					}
+				}
+
+				SECTION("Random") {
+					mt19937 g;
+					uniform_real_distribution<Data_t> dist;
+					generate(A.begin(), A.end(), [&g, &dist](){return dist(g);});
+					generate(x.begin(), x.end(), [&g, &dist](){return dist(g);});
+
+					blas_gemv_transposed(reinterpret_cast<FBLAS::MatrixVectorMultiplicationTransposed<Data_t, 16, 32, 16>::Col_t *>(A.data()), x.data(),
+					                     reinterpret_cast<FBLAS::MatrixVectorMultiplicationTransposed<Data_t, 16, 32, 16>::Col_t *>(y.data()),
+					                     N, M);
+
+					cblas_sgemv(CblasRowMajor, CblasTrans, M, N, 1, A.data(), N, x.data(), 1, 0, ref_y.data(), 1);
+
+					for (size_t i = 0; i < N; ++i) {
+						INFO("Y[" << i << "]");
+						CHECK(y[i] == Approx(ref_y[i]));
 					}
 				}
 			}
