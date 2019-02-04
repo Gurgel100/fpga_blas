b54161aa986d8ec2426c3186441a4439690290dc
Pascal Störzbach <stpascal@student.ethz.ch>
2019-02-04 16:28:15 +0100

    Added performance measurement script

--- test/PerfDot.cpp
+++ test/PerfDot.cpp
@@ -11,6 +11,7 @@
 #include <algorithm>
 #include <thread>
 #include <chrono>
+#include <cblas.h>
 #include "hlslib/SDAccel.h"
 
 using namespace std;
@@ -18,40 +19,15 @@ using namespace hlslib;
 
 using Type_t = float;
 
-template <class _Period>
-struct Frequency {
-	typedef _Period period;
-
-	explicit Frequency(double freq) : freq(freq) {}
-
-	template <class periodOther>
-	explicit Frequency(const Frequency<periodOther> &other) {
-		freq = other.template getFreq<period>();
-	}
-
-	template <class outPeriod = period>
-	double getFreq() const {
-		typedef ratio_divide<period, outPeriod> r;
-		return freq * r::num / r::den;
-	}
-
-private:
-	double freq;
-};
-
-const auto freq = Frequency<mega>(300.0);
-
 int main(int argc, const char *argv[]) {
-	const size_t size = static_cast<size_t>(argc > 1 ? strtod(argv[1], NULL) : 5e8);
-	if (size % 16 != 0) {
-		cerr << "Size must be divisible by 16!" << endl;
+	const size_t size = static_cast<size_t>(argc > 1 ? strtod(argv[1], NULL) : 5e6);
+	if (size % 32 != 0) {
+		cerr << "Size must be divisible by 32!" << endl;
 		return 1;
 	}
+	const std::string kernel_file = (argc > 2 ? argv[2] : "kernel_dot.xclbin");
 
-	vector<Type_t> inputHostX(size, 2), inputHostY(size, 1);
-	array<Type_t, 1> outputHost = {0};
-
-	cout << "size = " << size << endl;
+	vector<Type_t, ocl::AlignedAllocator<Type_t, 4096>> inputHostX(size, 2), inputHostY(size, 1), outputHost(1);
 
 	ocl::Context context;
 
@@ -59,25 +35,21 @@ int main(int argc, const char *argv[]) {
 	auto inputDeviceY = context.MakeBuffer<Type_t, ocl::Access::read>(ocl::MemoryBank::bank1, inputHostY.cbegin(), inputHostY.cend());
 	auto outputDevice = context.MakeBuffer<Type_t, ocl::Access::write>(ocl::MemoryBank::bank0, outputHost.begin(), outputHost.end());
 
-	auto program = context.MakeProgram("kernel_dot.xclbin");
-
-	cout << "Actual frequency: " << freq.getFreq<mega>() << " MHz" << endl;
+	auto program = context.MakeProgram(kernel_file);
 
 	auto kernel = program.MakeKernel("blas_dot", size, inputDeviceX, inputDeviceY, outputDevice);
 	cout << "Executing kernel..." << endl;
-	double time = 0.0;
-	double mintime = INFINITY;
-	double maxtime = -INFINITY;
-	for (int i = 0; i < 10; ++i) {
-		auto t = kernel.ExecuteTask();
-		time += t.first;
-		mintime = min(mintime, t.first);
-		maxtime = max(maxtime, t.first);
-	}
-	time /= 10;
+	auto time = kernel.ExecuteTask().first;
 
-	cout << "Kernel of size " << size << " executed in " << scientific << time << " seconds(" << mintime << "," << maxtime << ")" << endl;
-	cout << "Memory bandwidth: " << size * sizeof(Type_t) / time << " B/s" << endl;
+	outputDevice.CopyToHost(outputHost.begin());
+
+	auto res = cblas_sdot(size, inputHostX.data(), 1, inputHostY.data(), 1);
+	if (abs(res - outputHost[0]) > numeric_limits<Type_t>::epsilon() * (max(res, outputHost[0]))) {
+		cerr << "Got wrong result: " << outputHost[0] << " instead of " << res << endl;
+	}
+
+	cout << "csv_start" << endl << "size, time, read data, written data" << endl;
+	cout << size << ',' << time << ',' << size * sizeof(Type_t) * 2 << ',' << sizeof(Type_t) << endl;
 
 	return 0;
 }
---
+++ test/measure_performance.py
@@ -0,0 +1,70 @@
+import subprocess
+import sys
+from time import sleep
+
+run_timeout = 15 * 60
+
+if __name__ == "__main__":
+	if len(sys.argv) < 3:
+		print("Usage: measure_performance.py number_of_samples /path/to/testbinary [arguments]")
+		exit(1)
+
+	number_of_samples = int(sys.argv[1])
+	arguments = sys.argv[2:]
+
+	try:
+		read_data = 0
+		written_data = 0
+		total_time = 0.0
+		variance_time = 0.0
+		min_time = float("inf")
+		max_time = float("-inf")
+		additional_data = {}
+		for i in range(number_of_samples):
+			print("Running sample %i" % i)
+			tries = 0
+			while 1:
+				try:
+					sleep(2)
+					output_raw = subprocess.check_output(arguments, timeout=run_timeout)
+					break
+				except subprocess.TimeoutExpired:
+					tries += 1
+					if tries > 10:
+						print("Failed to execute kernel after 10 retries")
+						exit(4)
+			output = str(output_raw)
+			end_of_line = output.find("csv_start\\n")
+			if end_of_line == -1:
+				print("Output does not have the correct format")
+				exit(2)
+			csv_raw = output[end_of_line:]
+			rows = csv_raw.split("\\n")
+			headers = rows[1].split(",")
+			data = rows[2].split(",")
+			if len(headers) != len(data):
+				print("Output does have incorrect csv output")
+				exit(3)
+			for j in range(len(headers)):
+				header = headers[j].strip()
+				d = data[j].strip()
+				if header == "time":
+					t = float(d)
+					total_time += t
+					min_time = min(min_time, t)
+					max_time = max(max_time, t)
+					variance_time += t * t
+				elif i == 0:
+					if header == "read data":
+						read_data = int(d)
+					elif header == "written data":
+						written_data = int(d)
+					else:
+						additional_data[header] = d
+
+		print("samples, read data, written data, avg time, min time, max time, variance time, %s" % ", ".join(additional_data.keys()))
+		print(number_of_samples, read_data, written_data, total_time / number_of_samples, min_time, max_time, variance_time, sep=", ", end=", ")
+		print(", ".join(additional_data.values()))
+
+	except subprocess.CalledProcessError as e:
+		print("Failed to execute command: %s" % e)
